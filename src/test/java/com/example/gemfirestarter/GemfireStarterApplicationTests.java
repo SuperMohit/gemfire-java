b0031530b8fdcffc08daa899b48cf518fdf9bc00
qmohit-code <mohit@quest1.io>
2025-05-19 11:57:20 +0530

    Test PR

--- src/test/java/com/example/gemfirestarter/GemfireStarterApplicationTests.java
+++ src/test/java/com/example/gemfirestarter/GemfireStarterApplicationTests.java
@@ -9,6 +9,8 @@ class GemfireStarterApplicationTests {
     @Test
     void contextLoads() {
         // This test ensures that the Spring application context loads successfully
+        // and that all the necessary beans are created.
+        // You can add more specific tests here to verify the functionality of your application.
     }
 
 }
